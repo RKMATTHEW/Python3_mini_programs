d2443145d0d7a2de199f1cad0a6822f9d754d9f4
RKMATTHEW <107374438+RKMATTHEW@users.noreply.github.com>
2023-02-19 01:42:43 -0500

    This file simply goes over the print command, while creating "strings" within the print command

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+print('Hello World')
+print('My name is Kevin')
+print('It is 1:32 am')
+print('Tomorrow will be Sunday')
+print('I love to hear the birds chirp')
+print('My dog Benji likes to go on walks')
+print('It is cold outside, I wish I had brought my coat')
