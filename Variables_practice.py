c3cac1d9310e1eb8107aa703aaa7236e84dedbea
RKMATTHEW <107374438+RKMATTHEW@users.noreply.github.com>
2023-02-19 12:35:22 -0500

    This file simply goes over the print command, while creating "strings" within the print command

---
+++ Variables_practice.py
@@ -0,0 +1,42 @@
+#Programming languages offer a method of storing data for reuse. If there is a greeting we want to present,
+# a date we need to reuse, or a user ID we need to remember we can create a variable which can store a value. In Python,
+# we assign variables by using the equals sign (=).
+
+message_string = "Hello there"
+# Prints "Hello there"
+print(message_string)
+
+#Variables can’t have spaces or symbols in their names other than an underscore (_).
+# They can’t begin with numbers but they can have numbers after the first letter (e.g., cool_variable_5 is OK).
+
+# Greeting
+message_string = "Hello there"
+print(message_string)
+
+# Farewell
+message_string = "Hasta la vista"
+print(message_string)
+
+#Above, we create the variable message_string, assign a welcome message,
+#and print the greeting. After we greet the user, we want to wish them goodbye.
+# We then update message_string to a departure message and print that out.
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "burrito"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "pasta"
+# Printing out dinner
+print("Dinner:")
+print(meal)
