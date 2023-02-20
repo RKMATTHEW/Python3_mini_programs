4a5d19f9ce6195f1eb05e4309c085a81c6a3eb1a
RKMATTHEW <107374438+RKMATTHEW@users.noreply.github.com>
2023-02-19 20:13:51 -0500

    This file simply goes over the print command, while creating "strings" within the print command

--- Numbers.py
+++ Numbers.py
@@ -75,3 +75,19 @@ print(9 ** 3)
 # We can even perform fractional exponents
 # 4 to the half power, or 2
 print(4 ** 0.5)
+
+
+
+#Python offers a companion to the division operator called the modulo operator.
+# The modulo operator is indicated by % and gives the remainder of a division calculation.
+# If the number is divisible, then the result of the modulo operator will be 0.
+
+# Prints 4 because 29 / 5 is 5 with a remainder of 4
+print(29 % 5)
+
+# Prints 2 because 32 / 3 is 10 with a remainder of 2
+print(32 % 3)
+
+# Modulo by 2 returns 0 for even numbers and 1 for odd numbers
+# Prints 0
+print(44 % 2)
