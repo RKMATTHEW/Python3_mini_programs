07bcd5ce29e1b9cc0ec8d89f95c33327141c4169
RKMATTHEW <107374438+RKMATTHEW@users.noreply.github.com>
2023-03-02 20:11:50 -0500

    This file simply goes over the print command, while creating "strings" within the print command

--- Variables_practice.py
+++ Variables_practice.py
@@ -41,3 +41,97 @@ meal = "pasta"
 print("Dinner:")
 print(meal)
 
+
+
+#The + operator doesn’t just add two numbers, it can also “add” two strings!
+# The process of combining two strings is called string concatenation.
+# Performing string concatenation creates a brand new string comprised of
+# the first string’s contents followed by the second string’s contents
+# (without any added space in-between).
+
+
+greeting_text = "Hey there!"
+question_text = "How are you doing?"
+full_text = greeting_text + question_text
+
+# Prints "Hey there!How are you doing?"
+print(full_text)
+
+full_text = greeting_text + " " + question_text
+
+# Prints "Hey there! How are you doing?"
+print(full_text)
+
+
+
+
+#If you want to concatenate a string with a number you will need to make the number a string first,
+# using the str() function. If you’re trying to print() a numeric variable you can use commas to pass it
+# as a different argument rather than converting it to a string.
+
+birthday_string = "I am "
+age = 10
+birthday_string_2 = " years old today!"
+
+# Concatenating an integer with strings is possible if we turn the integer into a string first
+full_birthday_string = birthday_string + str(age) + birthday_string_2
+
+# Prints "I am 10 years old today!"
+print(full_birthday_string)
+
+# If we just want to print an integer
+# we can pass a variable as an argument to
+# print() regardless of whether
+# it is a string.
+
+# This also prints "I am 10 years old today!"
+print(birthday_string, age, birthday_string_2)
+
+
+
+
+
+#Python offers a shorthand for updating variables.
+# When you have a number saved in a variable and want to add to the current value of the variable,
+# you can use the += (plus-equals) operator.
+
+# First we have a variable with a number saved
+number_of_miles_hiked = 12
+
+# Then we need to update that variable
+# Let's say we hike another two miles today
+number_of_miles_hiked += 2
+
+# The new value is the old value
+# Plus the number after the plus-equals
+print(number_of_miles_hiked)
+# Prints 14
+
+#The plus-equals operator also can be used for string concatenation, like so:
+
+hike_caption = "What an amazing time to walk through nature!"
+
+# Almost forgot the hashtags!
+hike_caption += " #nofilter"
+hike_caption += " #blessed"
+
+#We create the social media caption for the photograph of nature we took on our hike,
+# but then update the caption to include important social media tags we almost forgot.
+
+
+
+
+#Python strings are very flexible, but if we try to create a string that occupies multiple lines
+# we find ourselves face-to-face with a SyntaxError. Python offers a solution: multi-line strings.
+# By using three quote-marks (""" or ''') instead of one,
+# we tell the program that the string doesn’t end until the next triple-quote.
+# This method is useful if the string being defined contains a lot of quotation marks and
+# we want to be sure we don’t close it prematurely.
+
+leaves_of_grass = """
+Poets to come! orators, singers, musicians to come!
+Not to-day is to justify me and answer what I am for,
+But you, a new brood, native, athletic, continental, greater than
+  before known,
+Arouse! for you must justify me.
+"""
