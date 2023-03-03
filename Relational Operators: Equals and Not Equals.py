6089f5428c67ca8ad6964fc8515c387fbe2ec3bc
RKMATTHEW <107374438+RKMATTHEW@users.noreply.github.com>
2023-03-02 21:20:52 -0500

    This file simply goes over the print command, while creating "strings" within the print command

--- Relational Operators: Equals and Not Equals.py
+++ Relational Operators: Equals and Not Equals.py
@@ -1,9 +1,12 @@
 #A boolean expression is a statement that can either be True or False.
 
+set_to_true = True
+set_to_false = False
+
+
+
 #Relational operators compare two items and return either True or False.
 # For this reason, you will sometimes hear them called comparators.
-
-
 
 1 == 1  # True
 
@@ -12,3 +15,4 @@
 3 == 5  # False
 
 '7' == 7  # False
+
