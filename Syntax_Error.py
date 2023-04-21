adf1d0bba7c328102c041e8017f1c57cf7a5a6f9
RKMATTHEW <107374438+RKMATTHEW@users.noreply.github.com>
2023-04-21 10:54:57 -0400

    Introduction to Bugs “First actual case of bug being found.“
    The story goes that on September 9th, 1947, computer scientist Grace Hopper found a moth in the Harvard Mark II computer’s logbook and reported the world’s first literal computer bug. However, the term “bug,” in the sense of technical error, dates back at least to 1878 and with Thomas Edison.
    
    Python refers to the mistakes within the program as errors and will point to the location where an error occurred with a ^ character. When programs throw errors that we didn’t expect to encounter, we call those errors bugs. Programmers call the process of updating the program so that it no longer produces bugs debugging.
    
    During your programming journey, you are destined to encounter innumerable red errors. Some even say that 75% of development time is spent on debugging. But what makes a programmer successful isn’t avoiding errors, it’s knowing how to solve them. And a good place to start is understanding what they are.
    
    In Python, there are many different ways of classifying errors, but here are some common ones:
    
    SyntaxError: Error caused by not following the proper structure (syntax) of the language.
    NameError: Errors reported when the interpreter detects a variable that is unknown.
    TypeError: Errors thrown when an operation is applied to an object of an inappropriate type.
    In this mini-lesson, we will be looking at these different error messages, and you’ll get some practice by debugging them one by one!
    
    Syntax Errors
    When we are writing Python programs, the interpreter is our first line of defense against errors.
    
    SyntaxError means there is something wrong with the way your program is written — punctuation that does not belong, a command where it is not expected, or a missing parenthesis can all trigger a SyntaxError.
    
    Here’s an example of a SyntaxError error message:
    
    File "script.py", line 1
      print(Hello world!)
                      ^
    SyntaxError: invalid syntax
    The interpreter will tell us where (the file name and line number) it got into trouble and its best guess as to what is wrong.
    
    After reading the error message, we can assume that the cause for this error is a lack of quotation marks around Hello world!.
    
    A NameError is reported by the Python interpreter when it detects a variable that is unknown.
    
    This can occur if a variable is used before it has been assigned a value or if a variable name is spelled differently than the point at which it was defined. The Python interpreter will display the line of code where the NameError was detected and indicate which name is found that was not defined.
    
    Here’s an example of a NameError error message:
    
    File "script.py", line 1, in <module>
        print(score)
    NameError: name 'score' is not defined
    
    This error is suggesting that the variable score was never defined in the program. Oops.
    
    A TypeError is reported by the Python interpreter when an operation is applied to a variable of an inappropriate type.
    
    This can occur in Python when one attempts to use an operator on something of the incorrect type.
    
    For example, let’s see what happens when we try and add together two incompatible types:
    
    piggy_bank = '2' + 0.25
    There will be an TypeError error message:
    
    Traceback (most recent call last):
      File "script.py", line 1, in <module>
        piggy_bank = '2' + 0.25
    TypeError: must be str, not float

---
+++ Syntax_Error.py
@@ -0,0 +1,75 @@
+#In script.py, your coworker Carolyn wrote a Fortune Cookie program that is supposed to print out a possible fortune based on a random number and an if/elif/else statement.
+
+#Run the program to check it out.
+
+# Fortune Cookie Program 🥠
+
+import random
+
+fortune = random.randint(0, 4)
+
+if fortune == 0
+  print("May you one day be carbon neutral")
+elif fortune == 1:
+  print("You have rice in your teeth")
+eli fortune == 2:
+  print("No snowflake feels responsible for an avalanche")
+elif fortune == 3:
+  print("You can only connect the dots looking backwards"
+elif fortune == 4:
+  print("The fortune you seek is in another cookie")
+
+#In script.py, another teammate Alex wrote a Who Wants to Be A Millionaire question and four options. If the answer is an uppercase or lowercase “A”, then the score goes up.
+
+#Run the program to check it out.
+
+# Who Wants To Be A Millionaire 💰
+
+score = 0
+
+option1 = 'Fresca'
+option2 = 'V8'
+option4 = 'A&W'
+
+print(
+    "For ordering his favorite beverages on demand, LBJ had four buttons installed in the Oval Office labeled 'Coffee', 'Tea', 'Coke', and what?\n")
+
+print("A.", option1)
+print("B.", option2)
+print("C.", option3)
+print("D.", option4)
+
+answer = 'a'
+
+if answer == 'A' or answer == 'a':
+    scor += 100
+print("\nCorrect!")
+else:
+print("\nNope, sorry!")
+
+#The word got out in the office that you are a pro bug catcher!
+
+#Another peer Alisha pops out of the blue and hands you a program that calculates the area of a triangle, a rectangle, and a circle.
+
+#Run the program to check it out.
+
+# Area Calculator 📏
+
+import math
+
+base = 20
+height = 30
+area = base * height / 2
+
+print("The triangle area is", area)
+
+length = 2
+width = 12
+area = length * width
+
+print("The rectangle area is" + area)
+
+radius = 36
+area = math.pi * radius * radius
+
+print("The circle area is", area)
