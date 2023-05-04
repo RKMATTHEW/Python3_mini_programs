43fba8f6d96fa23c25b3ce2e8b1f2b544a12f83f
RIVEVIL <107374438+RKMATTHEW@users.noreply.github.com>
2023-05-04 15:02:53 -0400

    Create list.py
    In Python, we can create a variable called heights to store these integers into a list:
    
    heights = [61, 70, 67, 64]
    Notice that:
    
    A list begins and ends with square brackets ([ and ]).
    Each item (i.e., 67 or 70) is separated by a comma (,)
    
    
    What can a List contain?
    Lists can contain more than just numbers.
    
    Let’s revisit our classroom example with heights:
    
    Noelle is 61 inches tall
    Ava is 70 inches tall
    Sam is 67 inches tall
    Mia is 64 inches tall
    Instead of storing each student’s height, we can make a list that contains their names:
    
    names = ["Noelle", "Ava", "Sam", "Mia"]
    We can even combine multiple data types in one list. For example, this list contains both a string and an integer:
    
    mixed_list_string_number = ["Noelle", 61]
    Lists can contain any data type in Python! For example, this list contains a string, integer, boolean, and float.
    
    mixed_list_common = ["Mia", 27, False, 0.5]
    
    A list doesn’t have to contain anything. You can create an empty list like this:
    
    empty_list = []
    Why would we create an empty list?
    
    Usually, it’s because we’re planning on filling it up later based on some other input. We’ll talk about two ways of filling up a list in the next exercise.
    
    Let’s practice writing an empty list!
    
    
    
    As we start exploring lists further in the next exercises, we will encounter the concept of a method.
    
    In Python, for any specific data-type ( strings, booleans, lists, etc. ) there is built-in functionality that we can use to create, manipulate, and even delete our data. We call this built-in functionality a method.
    
    For lists, methods will follow the form of list_name.method(). Some methods will require an input value that will go between the parenthesis of the method ( ).
    
    An example of a popular list method is .append(), which allows us to add an element to the end of a list.
    
    append_example = [ 'This', 'is', 'an', 'example']
    append_example.append('list')
     
    print(append_example)
    Will output:
    
    ['This', 'is', 'an', 'example', 'list']
    
    
    
    We can add a single element to a list using the .append() Python method.
    
    Suppose we have an empty list called garden:
    
    garden = []
    We can add the element "Tomatoes" by using the .append() method:
    
    garden.append("Tomatoes")
     
    print(garden)
    Will output:
    
    ['Tomatoes']
    We see that garden now contains "Tomatoes"!
    
    When we use .append() on a list that already has elements, our new element is added to the end of the list:
    
    # Create a list
    garden = ["Tomatoes", "Grapes", "Cauliflower"]
     
    # Append a new element
    garden.append("Green Beans")
    print(garden)
    Will output:
    
    ['Tomatoes', 'Grapes', 'Cauliflower', 'Green Beans']
    
    
    
    When we want to add multiple items to a list, we can use + to combine two lists (this is also known as concatenation).
    
    Below, we have a list of items sold at a bakery called items_sold:
    
    items_sold = ["cake", "cookie", "bread"]
    Suppose the bakery wants to start selling "biscuit" and "tart":
    
    items_sold_new = items_sold + ["biscuit", "tart"]
    print(items_sold_new)
    Would output:
    
    ['cake', 'cookie', 'bread', 'biscuit', 'tart']
    In this example, we created a new variable, items_sold_new, which contained both the original items sold, and the new items. We can inspect the original items_sold and see that it did not change:
    
    print(items_sold)
    Would output:
    
    ['cake', 'cookie', 'bread']
    We can only use + with other lists. If we type in this code:
    
    my_list = [1, 2, 3]
    my_list + 4
    we will get the following error:
    
    TypeError: can only concatenate list (not "int") to list
    If we want to add a single element using +, we have to put it into a list with brackets ([]):
    
    my_list + [4]

---
+++ list.py
@@ -0,0 +1,80 @@
+#Examine the existing list heights in your code editor.
+
+#A new student just joined the class!
+
+#Chloe is 65 inches tall
+
+#Add Chloe’s height to the end of the list heights.
+
+heights = [61, 70, 67, 64, 65]
+
+broken_heights = [65, 71, 59, 62]
+
+
+
+
+
+
+
+#Add any additional string to the end of the list ints_and_strings.
+
+#Create a new list called sam_height_and_testscore that contains:
+
+#The string "Sam" (to represent Sam’s name)
+#The number 67 (to represent Sam’s height)
+#The float 85.5 (to represent Sam’s score)
+#The boolean True (to represent Sam passing the test)
+#Make sure to write the elements in exact order.
+
+ints_and_strings = [1, 2, 3, "four", "five", "six"]
+
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
+
+
+
+
+
+
+#Jiho works for a gardening store called Petal Power. Jiho keeps a record of orders in a list called orders.
+
+#Use print to inspect the orders he has received today.
+
+#Jiho just received a new order for "tulips". Use append to add this string to orders.
+
+#Another order has come in! Use append to add "roses" to orders.
+
+#Use print to inspect the orders Jiho has received today.
+
+orders = ["daisies", "periwinkle"]
+
+print(orders)
+
+orders.append("tulips")
+
+orders.append("roses")
+
+print(orders)
+
+
+
+
+
+
+
+
+#Jiho is updating a list of orders. He just received orders for "lilac" and "iris".
+
+#Create a list called new_orders that contains our new orders.
+
+#Use + to create a new list called orders_combined that combines orders with new_orders.
+
+
+orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+
+# Create new orders here:
+
+new_orders = ["lilac", "iris"]
+
+orders_combined = orders + new_orders
+
+broken_prices = [5, 3, 4, 5, 4] + [4]
