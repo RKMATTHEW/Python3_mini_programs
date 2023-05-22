f0243bb5fbbfe06b0be54ac9b23426ff186ec02e
RKMATTHEW <107374438+RKMATTHEW@users.noreply.github.com>
2023-05-22 10:06:50 -0400

    In programming, it is common to want to work with collections of data. In Python, a list is one of the many built-in data structures that allows us to work with a collection of data in sequential order.
    Suppose we want to make a list of the heights of students in a class:
    
    Noelle is 61 inches tall
    Ava is 70 inches tall
    Sam is 67 inches tall
    Mia is 64 inches tall
    In Python, we can create a variable called heights to store these integers into a list:
    
    heights = [61, 70, 67, 64]
    Notice that:
    
    A list begins and ends with square brackets ([ and ]).
    Each item (i.e., 67 or 70) is separated by a comma (,)
    It’s considered good practice to insert a space () after each comma, but your code will run just fine if you forget the space.
    
    Instead of storing each student’s height, we can make a list that contains their names:
    
    names = ["Noelle", "Ava", "Sam", "Mia"]
    
    We can even combine multiple data types in one list. For example, this list contains both a string and an integer:
    
    mixed_list_string_number = ["Noelle", 61]
    
    Lists can contain any data type in Python! For example, this list contains a string, integer, boolean, and float.
    
    mixed_list_common = ["Mia", 27, False, 0.5]
    
    Empty Lists
    A list doesn’t have to contain anything. You can create an empty list like this:
    
    empty_list = []
    Why would we create an empty list?
    
    Usually, it’s because we’re planning on filling it up later based on some other input. We’ll talk about two ways of filling up a list in the next exercise.
    
    As we start exploring lists further in the next exercises, we will encounter the concept of a method.
    
    In Python, for any specific data-type ( strings, booleans, lists, etc. ) there is built-in functionality that we can use to create, manipulate, and even delete our data. We call this built-in functionality a method.
    
    For lists, methods will follow the form of list_name.method(). Some methods will require an input value that will go between the parenthesis of the method ( ).
    
    An example of a popular list method is .append(), which allows us to add an element to the end of a list.
    
    append_example = [ 'This', 'is', 'an', 'example']
    append_example.append('list')
    
    print(append_example)
    Will output:
    
    ['This', 'is', 'an', 'example', 'list']
    
    We can add a single element to a list using the .append() Python method.
    
    Suppose we have an empty list called garden:
    
    garden = []
    We can add the element "Tomatoes" by using the .append() method:
    
    garden.append("Tomatoes")
    
    print(garden)
    Will output:
    
    ['Tomatoes']

---
+++ Intro To lists.py
@@ -0,0 +1,19 @@
+heights = [61, 70, 67, 64, 65]
+
+broken_heights = [65, 71, 59, 62]
+
+ints_and_strings = [1, 2, 3, "four", "five"]
+
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
+
+my_empty_list = []
+
+orders = ["daisies", "periwinkle"]
+
+print(orders)
+
+orders.append("tulips")
+
+orders.append("roses")
+
+print(orders)
